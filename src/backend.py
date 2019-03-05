a172d8e1e9765296adb5e9ad5e492d728bac6c91
Dig Bik <jchan43@buffalo.edu>
2019-03-05 18:44:16 -0500

    added python stuff

---
+++ src/backend.py
@@ -0,0 +1,20 @@
+def getName(playerNames):
+    return playerNames
+
+def Scores(playerNames):
+    playerScores = []
+    for name in getName(playerNames):
+        name.split(" ")
+        playerScores.append(name)
+    return playerScores
+
+# def checkForFive():
+#     print("fuck")
+
+
+
+playerNames = ["Justin", "Justin", "Melinda", "Sighnda"]
+print(Scores(playerNames))
+
+
+
